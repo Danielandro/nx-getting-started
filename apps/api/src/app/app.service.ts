78e8f8ab27ee4fd0cc6145bd7c27333c56c6578f
Danny <mtme13@hotmail.com>
2020-05-17 17:47:53 +0100

    Add get + post api endpoints * both now work

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -1,8 +1,17 @@
+// business logic goes here
 import { Injectable } from '@nestjs/common';
 
+interface Todo {
+  title: string;
+}
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Welcome to api!' };
+  private todos: Todo[] = [{ title: 'Todo 1' }, { title: 'Todo 2' }];
+  getData(): Todo[] {
+    return this.todos;
+  }
+
+  addTodo() {
+    this.todos.push({ title: `Todo ${Math.floor(Math.random() * 1000)}` });
   }
 }
